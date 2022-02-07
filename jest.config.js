02ef506012c146fec7c6f25c97533f10f8eca879
Al <jibran.web@gmail.com>
2022-02-08 00:54:34 +0530

    Changed a .github/workflows to install cypress in submodule and edit e2e/tsconfig.json and jest.config.js to stop the ci error

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+	roots: ['<rootDir>/src'],
+	testMatch: [
+		'<rootDir>/src/__tests__/**/*.+(ts|tsx|js)',
+		'<rootDir>/src/?(*.)+(spec|test).+(ts|tsx|js)',
+	],
+	transform: {
+		'^.+\\.(ts|tsx)$': 'ts-jest',
+	},
 	preset: 'ts-jest',
 	testEnvironment: 'jest-environment-jsdom',
 	testPathIgnorePatterns: ['<rootDir>/e2e/'],
