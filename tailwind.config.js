a400189f41763b0206536886e6317c838b0c42af
Al <jibran.web@gmail.com>
2022-02-08 15:25:40 +0530

    Change screen sizes for all the screen classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		screens: {
+			'sm': '500px',
+			'md': '640px',
+			'lg': '768px',
+			'xl': '1024px',
+			'2xl': '1280px'
+		}
 	},
 	plugins: [],
 };
