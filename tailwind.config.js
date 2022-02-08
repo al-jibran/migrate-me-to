ff64531da90fe19affa76a94b2766ffab4780dc5
Al <jibran.web@gmail.com>
2022-02-09 04:46:13 +0530

    Add Poppins font as the base font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
-
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		screens: {
-			'sm': '500px',
-			'md': '640px',
-			'lg': '768px',
-			'xl': '1024px',
-			'2xl': '1280px'
+			sm: '500px',
+			md: '640px',
+			lg: '768px',
+			xl: '1024px',
+			'2xl': '1280px',
 		},
 		colors: {
 			white: '#ffffff',
 			black: '#000000',
 			gray: {
 				200: '#222222',
-				900: '#aaaaaa'
-			}
-		}
+				900: '#aaaaaa',
+			},
+		},
+		fontFamily: {
+			sans: ['Poppins', 'sans-serif'],
+		},
 	},
 	plugins: [],
 };
