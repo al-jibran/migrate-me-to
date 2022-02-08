d4080054d3c2581265b0321dd2e44df72e3a2528
Al <jibran.web@gmail.com>
2022-02-09 00:01:52 +0530

    Add unit test to verify the function is called when the hamburger menu is pressed

--- src/__tests__/components/navigation.spec.tsx
+++ src/__tests__/components/navigation.spec.tsx
@@ -1,17 +1,35 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Menu } from '../../components/Navigation';
 
 describe('Navigation Menu', () => {
 	describe('Smaller Screens', () => {
-		it('closes the menu on pressing on x', () => {
-			const handleCloseMenu = jest.fn();
+		let handleCloseMenu: jest.Mock;
+		let handleOpenMenu: jest.Mock;
+		let context: RenderResult;
 
-			const { getByTestId } = render(
-				<Menu handleCloseMenu={handleCloseMenu} />
+		beforeEach(() => {
+			handleCloseMenu = jest.fn();
+			handleOpenMenu = jest.fn();
+
+			context = render(
+				<Menu
+					handleCloseMenu={handleCloseMenu}
+					handleOpenMenu={handleOpenMenu}
+				/>
 			);
+		});
+
+		it('calls the handling function on pressing x', () => {
+			const { getByTestId } = context;
 			userEvent.click(getByTestId('close-menu'));
 			expect(handleCloseMenu).toHaveBeenCalled();
+		});
+
+		it('calls the handling function on pressing the hamburger icon', () => {
+			const { getByTestId } = context;
+			userEvent.click(getByTestId('open-menu'));
+			expect(handleOpenMenu).toHaveBeenCalled();
 		});
 	});
 });
