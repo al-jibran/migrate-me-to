bc088b157ca539d3960d8562ad89e7e5d143331a
Al <jibran.web@gmail.com>
2022-02-09 04:45:23 +0530

    Add homepage with hero section to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Navigation from './components/Navigation';
-
+import Homepage from './routes/Homepage';
 export const App = () => {
-	return <div>
-		<Navigation />
-	</div>;
+	return (
+		<div className='mt-5'>
+			<Navigation />
+			<div className='px-6'>
+				<Homepage />
+			</div>
+		</div>
+	);
 };
 
 export default App;
