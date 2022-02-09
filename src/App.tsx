0470e0dcbe36c255060f05663b65878d4752c140
Al <jibran.web@gmail.com>
2022-02-10 01:46:31 +0530

    Complete Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { IconProps } from '@iconify/react';
+import React, { Suspense } from 'react';
 import Navigation from './components/Navigation';
 import Homepage from './routes/Homepage';
 
 // For dark mode: linear-gradient(to right, #000000, #434343); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
 const footerStyles = {
-	mobile: 'mt-8 h-80 bg-gradient-to-r from-[#e1fad4] to-[#72b39f] p-10 w-full',
+	mobile:
+		'mt-8 h-80 relative bottom-0 bg-gradient-to-r from-[#e1fad4] to-[#72b39f] p-10',
 	sm: 'sm:text-black',
 };
 
 export const App = () => {
 	return (
 		<>
-			<div>
+			<div className='overflow-x-hidden h-full'>
 				<Navigation />
 				<Homepage />
 				<Footer />
@@ -23,7 +26,9 @@ export const App = () => {
 const Footer = () => {
 	const styles: string[] = Object.values(footerStyles);
 	const className = styles.join(' ');
-	console.log(className);
+	const Icon = React.lazy(() =>
+		import('@iconify/react').then(({ Icon }) => ({ default: Icon }))
+	);
 
 	return (
 		<div className={className}>
@@ -31,11 +36,58 @@ const Footer = () => {
 
 			<p className='underline my-5'>While you&apos;re here check out my:</p>
 			<ul>
-				<li>Portfolio</li>
-				<li>LinkedIn</li>
-				<li>Github</li>
-				<li>Twitter</li>
+				<SocialItem
+					Icon={Icon}
+					iconName='carbon:document-sentiment'
+					siteInfo={{ name: 'Portfolio', url: '#' }}
+				/>
+				<SocialItem
+					Icon={Icon}
+					iconName='logos:linkedin-icon'
+					siteInfo={{ name: 'LinkedIn', url: '#' }}
+				/>
+				<SocialItem
+					Icon={Icon}
+					iconName='logos:github-icon'
+					siteInfo={{ name: 'Github', url: 'https://www.github.com/al-jibran' }}
+				/>
+				<SocialItem
+					Icon={Icon}
+					iconName='logos:twitter'
+					siteInfo={{
+						name: 'Twitter',
+						url: 'https://www.twitter.com/Hi-Flexive',
+					}}
+				/>
 			</ul>
+		</div>
+	);
+};
+
+interface SocialItem {
+	Icon: React.LazyExoticComponent<
+		(
+			props: IconProps
+		) => React.ReactElement<
+			IconProps,
+			string | React.JSXElementConstructor<unknown>
+		>
+	>;
+	siteInfo: { name: string; url: string };
+	iconName: string;
+}
+
+const SocialItem: React.FC<SocialItem> = ({ Icon, siteInfo, iconName }) => {
+	return (
+		<div>
+			<li className='flex items-center'>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Icon icon={iconName} fontSize={30} />
+				</Suspense>
+				<a className='py-2 block w-full ml-5 text-lg' href={siteInfo.url}>
+					{siteInfo.name}
+				</a>
+			</li>
 		</div>
 	);
 };
