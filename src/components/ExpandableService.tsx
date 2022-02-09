c67d649ab1e2c7ce919401db18df5caced8eb3c7
Al <jibran.web@gmail.com>
2022-02-09 15:57:18 +0530

    Add code for service exapandables

--- src/components/ExpandableService.tsx
+++ src/components/ExpandableService.tsx
@@ -1,16 +1,18 @@
-import { TwitterLogo } from './svgs';
+import { TwitterLogo, Checkbox } from './svgs';
 
+const leftBox = () => 'w-1/6 p-3 mr-3 border-r-2 border-r-white';
 const ExpandableService = () => {
 	return (
-		<div>
+		<div className='my-6'>
 			<div className='flex max-h-24 bg-twitter'>
-				<TwitterLogo className='w-1/6 p-3 mr-3 border-r-2 border-r-white' />
-				<h3 className='self-center text-3xl font-bold text-white'>Twitter</h3>
+				<TwitterLogo className={leftBox()} />
+				<h3 className='self-center text-2xl font-bold text-white'>Twitter</h3>
 			</div>
 			<ul>
-				<li className="before:content-['*']">Friends</li>
-				<li>Liked</li>
-				<li>Followers</li>
+				<li className='flex'>
+					<Checkbox className={leftBox()} />
+					<div className='self-center text-xl'>Friends</div>
+				</li>
 			</ul>
 		</div>
 	);
