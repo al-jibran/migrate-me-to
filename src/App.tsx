86ccfb9594766c7a2dd2a0b684693f7b28303722
Al <jibran.web@gmail.com>
2022-02-09 14:35:55 +0530

    Complete Hero Section for mobiles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navigation from './components/Navigation';
 import Homepage from './routes/Homepage';
 export const App = () => {
 	return (
-		<div className='h-full overflow-x-hidden bg-blue-300'>
+		<div className='h-full overflow-x-hidden bg-gradient-to-tr from-[#ffffff] to-[#dbdbdb]'>
 			<Navigation />
 			<div className='text-gray-200 px-6 h-full pt-32'>
 				<Homepage />
--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,21 +1,15 @@
 import { DownArrow } from '../components/svgs';
 
 const Homepage = () => {
+	return <Hero />;
+};
+
+const Hero = () => {
 	return (
 		<section className='flex flex-col pb-5 h-full justify-between'>
 			<h1 className='font-bold text-7xl sm:text-8xl '>
 				Migrate to another account easily.
 			</h1>
-			{/* <a
-				href='#'
-				className='py-4 px-4
-				bg-gray-200 w-fit
-				text-white rounded-md
-				border-2
-				hover:bg-white
-				hover:text-black'>
-				See all offered services Todo
-			</a> */}
 			<DownArrow className='h-1/4 w-8 animate-bounce' />
 		</section>
 	);
