001ca4272d82f95e536ce33efe49f35275da2086
Al <jibran.web@gmail.com>
2022-02-13 02:09:38 +0530

    Add e2e test for service items

---
+++ e2e/cypress/integration/service.spec.ts
@@ -0,0 +1,27 @@
+describe('service', () => {
+	const serviceName = 'Twitter';
+
+	beforeEach(() => {
+		cy.server({ force404: true });
+		cy.visit('/');
+
+		cy.get('#services')
+			.scrollIntoView({ easing: 'linear' })
+			.find(`[data-testid=${serviceName}-service]`)
+			.as('service-item');
+	});
+	it('contains the service name', () => {
+		cy.get('@service-item').contains(serviceName);
+	});
+
+	it('has a hover effect on the service', () => {
+		cy.get('@service-item').realHover().should('have.css', 'cursor', 'pointer');
+	});
+
+	it('navigates to service page when clicked on a service', () => {
+		const serviceName = 'Twitter';
+		cy.get('@service-item').click();
+		cy.url().should('eq', `${Cypress.config().baseUrl}/service/${serviceName}`);
+		cy.contains(serviceName);
+	});
+});
