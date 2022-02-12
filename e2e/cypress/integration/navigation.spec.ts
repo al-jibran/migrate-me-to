844590cc872e205f7f9cbbaaca3a673c2173a8d4
Al <jibran.web@gmail.com>
2022-02-13 02:34:26 +0530

    add test to check the presence of navigation bar at all times

--- e2e/cypress/integration/navigation.spec.ts
+++ e2e/cypress/integration/navigation.spec.ts
@@ -12,32 +12,35 @@ const checkDarkToggle = async (element: JQuery<HTMLElement>): Promise<void> => {
 };
 
 describe('navigation', () => {
-	it('shows pointer cursor when hovered on logo', () => {
+	beforeEach(() => {
 		cy.server({ force404: true });
 		cy.visit('/');
-		const logo = cy.get('[aria-label=logo]');
-		logo
-			.trigger('mouseover')
-			.then((elem) => elem.hasClass('hover:cursor-pointer'));
+		const navigationBar = cy.get('nav');
+		navigationBar.should('be.visible');
 	});
 
-	it('takes the user to homepage when clicked on logo', () => {
-		cy.get('[aria-label=logo]').click();
-		cy.url().should('eq', `${Cypress.config().baseUrl}/`);
-	});
+	describe('navigation bar', () => {
+		it('shows pointer cursor when hovered on logo', () => {
+			const logo = cy.get('[aria-label=logo]');
+			logo
+				.trigger('mouseover')
+				.then((elem) => elem.hasClass('hover:cursor-pointer'));
+		});
 
-	it('toggles the dark mode', () => {
-		cy.get('h1').then(checkDarkToggle);
-		cy.get('h1').then(checkDarkToggle);
-	});
+		it('takes the user to homepage when clicked on logo', () => {
+			cy.get('[aria-label=logo]').click();
+			cy.url().should('eq', `${Cypress.config().baseUrl}/`);
+		});
 
-	it('initially has dark mode on', () => {
-		cy.clearLocalStorage();
-		cy.get('h1').should('have.css', 'color', 'rgb(255, 255, 255)');
-		cy.get('[aria-label="dark mode toggle"]').click();
-	});
+		it('toggles the dark mode', () => {
+			cy.get('h1').then(checkDarkToggle);
+			cy.get('h1').then(checkDarkToggle);
+		});
 
-	it('remembers the theme the user had set', () => {
-		cy.get('h1').should('have.css', 'color', 'rgb(34, 34, 34)');
+		it('initially has dark mode on', () => {
+			cy.clearLocalStorage();
+			cy.get('h1').should('have.css', 'color', 'rgb(255, 255, 255)');
+			cy.get('[aria-label="dark mode toggle"]').click();
+		});
 	});
 });
