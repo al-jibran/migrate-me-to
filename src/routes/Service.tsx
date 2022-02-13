f1dd8d5105ecd11816a9e459191c409b7658ef88
Al <jibran.web@gmail.com>
2022-02-13 19:43:35 +0530

    Fix minor bug with h1 border color having a black color instead of services

--- src/routes/Service.tsx
+++ src/routes/Service.tsx
@@ -1,24 +1,15 @@
 import { useParams } from 'react-router-dom';
-import {
-	StepFail,
-	StepInactive,
-	StepInProgress,
-	StepSuccess,
-} from '../components/svgs';
+// import {
+// 	StepFail,
+// 	StepInactive,
+// 	StepInProgress,
+// 	StepSuccess,
+// } from '../components/svgs';
 
 const styles: Record<string, Record<string, string>> = {
 	border: {
-		Twitter: 'border-twitter',
-		Reddit: 'border-reddit',
-	},
-
-	fill: {
-		Twitter: 'fill-twitter',
-		Reddit: 'fill-reddit',
-	},
-
-	marker: {
-		inActive: 'marker:content-[}]',
+		Twitter: 'border-b-twitter',
+		Reddit: 'border-b-reddit',
 	},
 };
 
@@ -31,10 +22,9 @@ const Service = () => {
 
 	if (!styles) {
 		throw new Error('No style was defined for this page');
-	} else if (!styles.border || !styles.fill) {
+	} else if (!styles.border) {
 		styles.border = {};
 		styles.border[name] = '#000';
-		styles.fill = {};
 	}
 
 	const borderColor = styles.border[name];
@@ -42,37 +32,39 @@ const Service = () => {
 	return (
 		<div className='pt-32 px-10 h-screen'>
 			<h1
-				className={`text-4xl ${borderColor} uppercase pb-3 border-b-4 border-white w-fit mx-auto`}>
+				className={`text-4xl uppercase pb-3 border-b-4 ${borderColor} w-fit mx-auto`}>
 				1<sup className='lowercase'>st</sup> Account
 			</h1>
-			<Steps />
+			<Steps onGoing={true} success={true} />
 		</div>
 	);
 };
 
-const Steps = () => {
+interface StepsProps {
+	onGoing: boolean;
+	success: boolean;
+}
+
+const Steps: React.FC<StepsProps> = ({ onGoing, success }) => {
+	onGoing && success;
 	return (
 		<>
 			<h2 className='mt-10 mb-4 uppercase'>Steps</h2>
 			<ul>
 				<li className='flex items-center py-1'>
-					<StepSuccess />
 					<p>
 						Sign in to the 1<sup>st</sup> account
 					</p>
 				</li>
 				<li className='flex items-center py-1'>
-					<StepFail />
 					<p>Select what to migrate</p>
 				</li>
 				<li className='flex items-center py-1'>
-					<StepInProgress />
 					<p>
 						Sign in to the 2<sup>nd</sup> account
 					</p>
 				</li>
 				<li className='flex items-center py-1'>
-					<StepInactive />
 					<p>Wait</p>
 				</li>
 			</ul>
