4010df283841efacfa6a6b55a5646b12c0c93f11
Al <jibran.web@gmail.com>
2022-02-13 15:52:19 +0530

    Refactor code for checking navigation at the top of the page and add an assertion to check the existence of nav element

--- e2e/cypress/integration/mobile.spec.ts
+++ e2e/cypress/integration/mobile.spec.ts
@@ -2,6 +2,8 @@ describe('navigation', () => {
 	describe('on mobile and tablets', () => {
 		beforeEach(() => {
 			cy.visit('/');
+			cy.get('nav').should('exist');
+			cy.get('[aria-label=open-menu]').as('open-menu');
 			cy.viewport(500, 600);
 		});
 
@@ -10,7 +12,7 @@ describe('navigation', () => {
 		});
 
 		it('hides the menu on tapping x', () => {
-			cy.get('[aria-label=open-menu]')
+			cy.get('@open-menu')
 				.click()
 				.then(() => {
 					cy.get('[aria-label=close-menu]').click();
@@ -19,14 +21,14 @@ describe('navigation', () => {
 		});
 
 		it('shows the menu on pressing on the hamburger icon', () => {
-			cy.get('[aria-label=open-menu]').click();
+			cy.get('@open-menu').click();
 			cy.get('[aria-label=menu]').should('be.slidingFromRight');
 		});
 
 		it('hides the menu when clicked on an item', () => {
 			cy.viewport(500, 600); // to be removed when desktop site is completed
 			cy.server({ force404: true });
-			cy.get('[aria-label=open-menu]').click();
+			cy.get('@open-menu').click();
 			cy.contains('Services').click().should('not.be.slidingFromRight');
 		});
 	});
