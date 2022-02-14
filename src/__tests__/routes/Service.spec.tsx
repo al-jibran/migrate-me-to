1ba6c349cb1e261d740715a34c20dbf83f3e1fa8
Al <jibran.web@gmail.com>
2022-02-14 23:56:19 +0530

    Remove test related to ui in cypress tests

--- src/__tests__/routes/Service.spec.tsx
+++ src/__tests__/routes/Service.spec.tsx
@@ -1,25 +1,35 @@
 import { render, RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ServiceType } from '../../data/services';
-import {
-	ServiceContainer,
-} from '../../routes/Service';
+import { ServiceContainer, StepStatusType } from '../../routes/Service';
 
 // Give a callback function to Steps componet that will take the name for in progress, complete, processes and update their state
 // check whether the Steps components calls it
 describe('Service', () => {
-	let context: RenderResult;
+	let context: RenderResult, handleDispatchStatus: jest.Mock;
+
+	const status = {
+		stepOne: StepStatusType.INACTIVE,
+		stepTwo: StepStatusType.INACTIVE,
+		stepThree: StepStatusType.INACTIVE,
+		stepFour: StepStatusType.INACTIVE,
+	};
 
 	const service: ServiceType = {
 		name: 'Twitter',
 		LogoSvgComponent: jest.fn().mockImplementation(() => null),
 		transferrableList: [],
 	};
-	
 
 	beforeEach(() => {
+		handleDispatchStatus = jest.fn();
 		context = render(
-			<ServiceContainer name={service['name']} service={service} />
+			<ServiceContainer
+				name={service['name']}
+				service={service}
+				status={status}
+				handleDispatchStatus={handleDispatchStatus}
+			/>
 		);
 	});
 	describe('initially', () => {
@@ -41,14 +51,14 @@ describe('Service', () => {
 			button = getByRole('button', {
 				name: /let's start!/i,
 			}) as HTMLButtonElement;
+			userEvent.click(button);
 		});
 
 		it('changes the first icon to be in progress the first time', () => {
-			const { getByLabelText } = context;
-
-			userEvent.click(button);
-
-			expect(getByLabelText('in progress')).not.toBeNull();
+			expect(handleDispatchStatus).toHaveBeenCalledWith(
+				'INPROGRESS',
+				'stepOne'
+			);
 		});
 	});
 });
