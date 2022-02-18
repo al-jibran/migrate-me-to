badd3a2747f90675345e657856cbcb7ff11f0eaa
Al <jibran.web@gmail.com>
2022-02-18 18:41:25 +0530

    Fix not scrolling to the top of the service page when clicked on an item

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import App from './App';
 import './assets/styles/main.css';
 import Homepage from './routes/Homepage';
 import Service from './routes/Service';
 
+const ScrollToTop = () => {
+	const pathName = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathName]);
+
+	return null;
+};
+
 ReactDOM.render(
 	<BrowserRouter>
+		<ScrollToTop />
 		<Routes>
 			<Route path='/' element={<App />}>
 				<Route index element={<Homepage />} />
