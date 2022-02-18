ae095ce1601ec69c9b83dd1e035a1f7e1cdff6c6
Al <jibran.web@gmail.com>
2022-02-18 16:55:44 +0530

    Refactor tests to separately test the DispatchStatus class and Service Component UI

--- src/__tests__/routes/Service.spec.tsx
+++ src/__tests__/routes/Service.spec.tsx
@@ -1,19 +1,10 @@
 import { render, RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ServiceType } from '../../data/services';
-import { ServiceContainer, StepStatusType } from '../../routes/Service';
+import { ServiceContainer } from '../../routes/Service';
 
-// Give a callback function to Steps componet that will take the name for in progress, complete, processes and update their state
-// check whether the Steps components calls it
 describe('Service', () => {
-	let context: RenderResult, handleDispatchStatus: jest.Mock;
-
-	const status = {
-		stepOne: StepStatusType.INACTIVE,
-		stepTwo: StepStatusType.INACTIVE,
-		stepThree: StepStatusType.INACTIVE,
-		stepFour: StepStatusType.INACTIVE,
-	};
+	let context: RenderResult;
 
 	const service: ServiceType = {
 		name: 'Twitter',
@@ -22,14 +13,8 @@ describe('Service', () => {
 	};
 
 	beforeEach(() => {
-		handleDispatchStatus = jest.fn();
 		context = render(
-			<ServiceContainer
-				name={service['name']}
-				service={service}
-				status={status}
-				handleDispatchStatus={handleDispatchStatus}
-			/>
+			<ServiceContainer name={service['name']} service={service} />
 		);
 	});
 	describe('initially', () => {
@@ -52,13 +37,6 @@ describe('Service', () => {
 				name: /let's start!/i,
 			}) as HTMLButtonElement;
 			userEvent.click(button);
-		});
-
-		it('changes the first icon to be in progress the first time', () => {
-			expect(handleDispatchStatus).toHaveBeenCalledWith(
-				'INPROGRESS',
-				'stepOne'
-			);
 		});
 	});
 });
