c8639643a831e17b8dfab01094a719f89cdce491
Al <jibran.web@gmail.com>
2022-02-19 02:02:22 +0530

    Write a failing test for generating request token header string

--- backend/src/__tests__/OAuthHeader.spec.ts
+++ backend/src/__tests__/OAuthHeader.spec.ts
@@ -1,9 +1,12 @@
 import OAuthHeader from '../utility/OAuthHeader';
 
 describe('OAuth Header', () => {
+	const validHeaderString =
+		/OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog", oauth_nonce=".+", oauth_signature=".+", oauth_signature_method="HMAC-SHA1", oauth_timestamp="\d+",( oauth_token=".+",)? oauth_version="1.0"/g;
+
 	it('returns a header string without token', () => {
-		const header = new OAuthHeader('abc', 'def');
-		header.getHeaderString();
-		expect(2).toBe(2);
+		const header = new OAuthHeader('xvz1evFS4wEEPTGEFPHBog', 'def');
+		const headerString = header.getHeaderString();
+		expect(headerString).toMatch(validHeaderString);
 	});
 });
