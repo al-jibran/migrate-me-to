9c2c169c05d9f0395206c98dbb7887560e316aa5
Al <jibran.web@gmail.com>
2022-02-21 00:01:29 +0530

    Write an e2e test that tests the clicking of login button on service page

--- e2e/cypress/integration/service.spec.ts
+++ e2e/cypress/integration/service.spec.ts
@@ -1,5 +1,7 @@
 describe('a service page', () => {
 	const serviceName = 'Twitter';
+	const serviceAuthorizeUrl =
+		/https:\/\/api\.twitter\.com\/oauth\/authorize\?oauth_token=\w+/g;
 
 	beforeEach(() => {
 		cy.visit(`/service/${serviceName}`);
@@ -26,5 +28,10 @@ describe('a service page', () => {
 		cy.get('#steps li:nth-child(2)').find('[aria-label="inactive"]');
 		cy.get('#steps li:nth-child(3)').find('[aria-label="inactive"]');
 		cy.get('#steps li:nth-child(4)').find('[aria-label="inactive"]');
+	});
+
+	it('open the page to login when clicked on log in to service button', () => {
+		cy.get('#login').click();
+		cy.url().should('match', serviceAuthorizeUrl);
 	});
 });
