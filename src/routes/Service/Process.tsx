31de3a62307166c45d26904c5cbd033643434e8b
Al <jibran.web@gmail.com>
2022-02-23 01:48:03 +0530

    Refactor Process UI elements into their own container

--- src/routes/Service/Process.tsx
+++ src/routes/Service/Process.tsx
@@ -1,4 +1,8 @@
 import serviceImage from 'images/sign-in-with-twitter.png';
+import { Dispatch, useState } from 'react';
+import { getAuthorizeUserLink } from '../../api';
+import { DispatchStatus } from './state/DispatchStatus';
+import { ReducerActionType } from './state/reducer';
 
 type StylesProps = {
 	borderBottom: Record<string, string>;
@@ -19,26 +23,56 @@ const styles: StylesProps = {
 
 interface ProcessProps {
 	name: string;
-	handleLogin: () => void;
-	loading: boolean;
+	dispatchStepsStatus: Dispatch<ReducerActionType>;
 }
 
 export const Process: React.FC<ProcessProps> = ({
 	name,
-	handleLogin,
+	dispatchStepsStatus,
+}) => {
+	const [loading, setLoading] = useState(false);
+
+	const { handleDispatchStatus } = new DispatchStatus(dispatchStepsStatus);
+
+	const handleLogin = () => {
+		handleDispatchStatus('INPROGRESS', 'stepOne');
+		setLoading(true);
+
+		getAuthorizeUserLink()
+			.then((data) => {
+				window.open(data.authorizeUrl, '_self');
+			})
+			.catch((e) => e);
+	};
+
+	return (
+		<ProcessContainer name={name} loading={loading} handleLogin={handleLogin} />
+	);
+};
+
+interface ProcessContainerProps {
+	name: string;
+	loading: boolean;
+	handleLogin: () => void;
+}
+
+export const ProcessContainer: React.FC<ProcessContainerProps> = ({
+	name,
 	loading,
+	handleLogin,
 }) => {
 	const headingBorderColor = styles.borderBottom[name];
 
 	return (
-		<div>
-			<h3
-				className={`uppercase border-b-4 ${headingBorderColor} w-fit mx-auto mt-14`}>
+		<div className='flex justify-between'>
+			<h3 className={`uppercase border-b-4 ${headingBorderColor} w-fit mt-14`}>
 				1<sup className='lowercase'>st</sup> Account
 			</h3>
-			<div className='flex justify-center mt-14'>
+			<div className='mt-14'>
 				{loading ? (
-					<span aria-label='loading'>Loading...</span>
+					<div
+						aria-label='loading'
+						className='border-twitter border-l-transparent border-r-transparent border-l-white animate-spin border-4 rounded-full w-8 h-8'></div>
 				) : (
 					<img
 						id='login'
