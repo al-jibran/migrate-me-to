536c9e64b492bfd5a2ca65710e4880fbe700ee0d
Al <jibran.web@gmail.com>
2022-02-24 13:58:50 +0530

    Remove test for ProcessContainer and instead test the Process component as a whole with mocked api

--- src/__tests__/routes/Service/Service.spec.tsx
+++ src/__tests__/routes/Service/Service.spec.tsx
@@ -44,90 +44,21 @@ describe('Service', () => {
 });
 
 describe('Process', () => {
-	let processContext: RenderResult;
+	let context: RenderResult;
 	const serviceName = 'Twitter';
 	const error = {
 		code: 503,
-		message: 'There is a problem connecting to the server. Try again later.',
+		message: 'There was a problem connecting to the server. Try again later.',
 	};
 
-	const renderWithProps = (propsOverride = {}) => {
-		const props = {
-			name: serviceName,
-			handleLogin: jest.fn().mockName('handleLogin'),
-			loading: false,
-			error: null,
-			...propsOverride,
-		};
 
-		processContext = render(<ProcessContainer {...props} />);
-	};
 
-	describe('while loading', () => {
-		beforeEach(() => {
-			renderWithProps({ loading: true });
+
+
+
+
 		});
 
-		it('displays a loading indicator', () => {
-			const { queryByLabelText } = processContext;
-			expect(queryByLabelText('loading')).not.toBeNull();
 		});
-
-		it('does not display the error message', () => {
-			const { queryByText } = processContext;
-			expect(queryByText(error.message)).toBeNull();
-		});
-
-		it('does not display the log in button', () => {
-			const { queryByRole } = processContext;
-			expect(queryByRole('link', { name: /log in/i })).toBeNull();
-		});
-	});
-
-	describe('error', () => {
-		beforeEach(() => {
-			renderWithProps({ error });
-			return act(flushPromises);
-		});
-
-		it('displays the appropriate message if cannot connect to the backend', () => {
-			const { queryByText } = processContext;
-			expect(queryByText(error.message)).not.toBeNull();
-		});
-
-		it('does not display the loading indicator', () => {
-			const { queryByLabelText } = processContext;
-			expect(queryByLabelText('loading')).toBeNull();
-		});
-
-		it('displays the button to log in again', () => {
-			const { queryByRole } = processContext;
-			expect(queryByRole('link', { name: /log in/i })).not.toBeNull();
-		});
-	});
-
-	it('does not display the loading indicator when not loading', () => {
-		renderWithProps(false);
-		const { queryByLabelText, queryByRole } = processContext;
-
-		expect(queryByLabelText('loading')).toBeNull();
-		expect(queryByRole('link', { name: /log in/i })).not.toBeNull();
-	});
-
-	it('changes the button to loading when the button is clicked', async () => {
-		const dispatch = jest.fn();
-
-		const { queryByRole } = render(
-			<Process name={serviceName} dispatchStepsStatus={dispatch} />
-		);
-
-		const button = queryByRole('link', { name: /log in/i }) as HTMLImageElement;
-
-		// using toBeInTheDocument instead of toBeNull because toBeNull would require a rerendering since
-		// this instance of button is when the button was not null.
-
-		expect(button).toBeInTheDocument();
-		userEvent.click(button);
-		expect(button).not.toBeInTheDocument();
 	});
 });
