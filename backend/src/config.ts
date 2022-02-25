853d4bda4f2f3f420b6fbfd25620ccc0566f644c
Al <jibran.web@gmail.com>
2022-02-25 16:25:34 +0530

    Remove mock server config from config files

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,6 @@
 import { config } from 'dotenv';
 config();
 
-export const PORT = process.env.PORT || 4000;
-export const MOCK_SERVER_PORT = process.env.MOCK_SERVER_PORT || 5000;
+export const PORT = process.env.PORT;
 
-export let TWITTER_BASE_URL = 'https://api.twitter.com';
-
-if (process.env.NODE_ENV === 'testing') {
-	TWITTER_BASE_URL = process.env.MOCK_SERVER || `http://localhost:${MOCK_SERVER_PORT}`;
-}
+export const TWITTER_BASE_URL = 'https://api.twitter.com';
