9f49dbc2bb2f7083769fbb662727a354ac4c8e0f
Al <jibran.web@gmail.com>
2022-02-25 16:26:37 +0530

    Extends express-session module and add current SessionData types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
-import app from './app';
 import http from 'http';
 import { PORT } from './config';
+import app from './app';
+
+declare module 'express-session' {
+	interface SessionData {
+		oauth_token: string;
+	}
+}
 
 const server = http.createServer(app);
 
