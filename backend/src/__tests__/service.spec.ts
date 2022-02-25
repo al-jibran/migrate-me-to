95dddb413259db020b98d64249ce81dd677efb6d
Al <jibran.web@gmail.com>
2022-02-25 23:28:24 +0530

    Write a test to check that /twitter/verify route returns a 204 when user is successfully authorized

--- backend/src/__tests__/service.spec.ts
+++ backend/src/__tests__/service.spec.ts
@@ -1,5 +1,5 @@
-import express, { Express, NextFunction } from 'express';
-import supertest, { SuperTest } from 'supertest';
+import supertest from 'supertest';
+import { stubApiForRoute } from './__utils__/stub-api';
 import { getOAuthToken } from '../api';
 import app from '../app';
 
@@ -10,25 +10,29 @@ const mockToken = 'NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0';
 const api = supertest(app);
 
 describe('Service', () => {
-	
 	it('returns a link to login to the service', async () => {
 		(getOAuthToken as jest.Mock).mockResolvedValue({
 			oauth_token: 'NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0',
 			oauth_callback_confirmed: true,
 		});
-		
+
 		const response = await api.get('/twitter/authorize').expect(200);
 		expect(getOAuthToken).toHaveBeenCalled();
-		
+
 		expect(response.body).toEqual({
 			authorizeUrl: `https://api.twitter.com/oauth/authorize?oauth_token=${mockToken}`,
 		});
 	});
 
-	it('return a successful status on acquiring a oauth_token', async () => {
-		await api
-			.get('/twitter/callback')
-			.expect(204)
-			.expect('Content-Type', /application\/json/);
+	describe('/verify', () => {
+		const fromRoute = (route: string, session: Record<string, any>) => {
+			return stubApiForRoute(app, route)
+				.withSession(session)
+				.get(`/twitter/verify`);
+		};
+
+		it('returns 204 when authorization is successful', async () => {
+			await fromRoute('/twitter/verify', { verified: true }).expect(204);
+		});
 	});
 });
