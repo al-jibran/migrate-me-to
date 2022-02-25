5720eb9d9b048dde6847477ade29883b82cc15de
Al <jibran.web@gmail.com>
2022-02-25 23:24:00 +0530

    Add a deafult port to fallback to

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 4000;
 
 export const TWITTER_BASE_URL = 'https://api.twitter.com';
