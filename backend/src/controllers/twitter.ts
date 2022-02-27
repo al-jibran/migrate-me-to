b330e387f6b9505693f2c25ab8b2e7db5164a695
Al <jibran.web@gmail.com>
2022-02-27 16:44:53 +0530

    Add a Twitter class that encapsulates login to handle services for twitter and move logic related to requesting oauth token out of /authorize to a method inside of the twitter class

--- backend/src/controllers/twitter.ts
+++ backend/src/controllers/twitter.ts
@@ -1,54 +1,24 @@
 import express from 'express';
-
-import OAuthHeader from '../utility/OAuthHeader';
-import { METHOD, Request } from '../utility/OAuthHeader/types';
-import { uriPercentEncode } from '../utility';
-import { TWITTER_BASE_URL } from '../config';
-import { getOAuthToken } from '../api';
+import Twitter from '../services/Twitter';
 
 const twitterRouter = express.Router();
 
-const encodedCallback = uriPercentEncode(
-	'http://127.0.0.1:4000/twitter/callback'
-);
+const twitter = new Twitter();
 
 twitterRouter.get('/authorize', async (req, res) => {
-	const apiKey = process.env.TWITTER_CONSUMER_KEY;
-	const secret = process.env.TWITTER_CONSUMER_KEY_SECRET;
-
-	if (!apiKey) {
-		throw new Error('The Consumer/API key was not provided');
-	}
-
-	if (!secret) {
-		throw new Error('The Consumer/API Secret was not provided');
-	}
-
-	const request: Request = {
-		method: METHOD.POST,
-		uri: `${TWITTER_BASE_URL}/oauth/request_token?oauth_callback=${encodedCallback}`,
-	};
-
+	// The OAuth process has started!
+	req.session.processing = true;
 	try {
-		req.session.processing = true;
-		// Make a request to twitter
-		const oAuthHeader = new OAuthHeader(apiKey, secret);
-		const headers = {
-			Authorization: oAuthHeader.getHeaderString(request),
-		};
-		const oAuthAuthorize = await getOAuthToken(request.uri, headers);
+		// Connect to twitter and get token
+		const oAuthAuthorize = await twitter.getAuthorizeToken(
+			'http://127.0.0.1:4000/twitter/callback'
+		);
 
-		// confirm callback
-		if (!oAuthAuthorize.oauth_callback_confirmed) {
-			throw new Error('Could not confirm callback for oauth.');
-		}
-
-		// set the token received in session
 		req.session.oauth_token = oAuthAuthorize.oauth_token;
-		const authorizeUserLink = `https://api.twitter.com/oauth/authorize?oauth_token=${oAuthAuthorize.oauth_token}`;
-		req.session.save();
 
 		// send the client uri for authorization
+		const authorizeUserLink = `https://api.twitter.com/oauth/authorize?oauth_token=${oAuthAuthorize.oauth_token}`;
+
 		res.json({
 			authorizeUrl: authorizeUserLink,
 		});
---
+++ backend/src/services/Twitter.ts
@@ -0,0 +1,73 @@
+import OAuthHeader from '../utility/OAuthHeader';
+import { METHOD } from '../utility/OAuthHeader/types';
+import { hasOwnProperty } from '../utility';
+import { Request } from '../utility/OAuthHeader/types';
+import { postWithHeader } from '../api';
+import { responseToObject } from '../utility';
+
+interface AuthorizeToken {
+	oauth_token: string;
+	oauth_token_secret: string;
+	oauth_callback_confirmed: boolean;
+}
+
+const isAuthorizedToken = (obj: AuthorizeToken | {}): obj is AuthorizeToken => {
+	return (
+		hasOwnProperty(obj, 'oauth_token') &&
+		hasOwnProperty(obj, 'oauth_token_secret') &&
+		hasOwnProperty(obj, 'oauth_callback_confirmed')
+	);
+};
+
+class Twitter {
+	private TWITTER_BASE_URL = 'https://api.twitter.com';
+	private oAuthHeader: OAuthHeader;
+
+	constructor() {
+		if (!process.env.TWITTER_CONSUMER_KEY) {
+			throw new Error('The Consumer/API key was not provided');
+		}
+
+		if (!process.env.TWITTER_CONSUMER_KEY_SECRET) {
+			throw new Error('The Consumer/API Secret was not provided');
+		}
+
+		const apiKey = process.env.TWITTER_CONSUMER_KEY;
+		const apiKeySecret = process.env.TWITTER_CONSUMER_KEY_SECRET;
+
+		this.oAuthHeader = new OAuthHeader(apiKey, apiKeySecret);
+	}
+
+	getAuthorizeToken = async (callbackUrl: string): Promise<AuthorizeToken> => {
+		const request: Request = {
+			method: METHOD.POST,
+			uri: `${this.TWITTER_BASE_URL}/oauth/request_token`,
+		};
+
+		const headers = {
+			Authorization: this.oAuthHeader.getHeaderString(request, {
+				oauth_callback: callbackUrl,
+			}),
+		};
+
+		try {
+			const authorizeString = await postWithHeader(request.uri, headers);
+			const parseAuthorizeString = responseToObject(authorizeString);
+
+			if (!isAuthorizedToken(parseAuthorizeString)) {
+				throw new Error(
+					'Did not receive the expected response for authorization from Twitter.'
+				);
+			}
+
+			if (!parseAuthorizeString.oauth_callback_confirmed) {
+				throw new Error('OAuth Callback could not be confirmed');
+			}
+
+			return parseAuthorizeString;
+		} catch (e) {
+			throw e;
+		}
+	};
+
+export default Twitter;
