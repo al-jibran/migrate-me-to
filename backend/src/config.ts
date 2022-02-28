38d823411a0bf1861dc67a4a75066ae79b2b5cb4
Al <jibran.web@gmail.com>
2022-02-28 17:38:30 +0530

    Set callback url in production with real backend

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,10 @@ config();
 
 export const PORT = process.env.PORT || 4000;
 
-export const CALLBACK_URL = 'http://127.0.0.1:4000/twitter/callback';
+export const CALLBACK_URL =
+	process.env.NODE_ENV === 'production'
+		? process.env.CALLBACK_URL
+		: 'http://127.0.0.1:4000/twitter/callback';
 
 export const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY;
 export const TWITTER_CONSUMER_KEY_SECRET = process.env.TWITTER_CONSUMER_KEY_SECRET;
